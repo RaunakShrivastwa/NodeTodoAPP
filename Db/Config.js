8ea8ea4f2130a34e3d002adb438f657a619faf26
shu2002k <shubhkuttiya@gmail.com>
2023-08-16 09:01:10 -0700

    commite

--- Db/Config.js
+++ Db/Config.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://Shubham:369silent@cluster0.zy4qbxi.mongodb.net/?retryWrites=true&w=majority');
+mongoose.connect('mongodb://127.0.0.1:27017/TodoApp');
+// mongoose.connect('mongodb+srv://Shubham:369@cluster0.zy4qbxi.mongodb.net/?retryWrites=true&w=majority');
+
 const db=mongoose.connection;
 
 db.on('error',console.error.bind(console,"There is problem with connection"))
