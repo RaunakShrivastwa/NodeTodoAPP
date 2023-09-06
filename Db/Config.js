8c6004f43700fe8b099c8322a20e183cf1fd6f99
shu2002k <shubhkuttiya@gmail.com>
2023-09-05 19:53:22 -0700

    COMMITE

--- Db/Config.js
+++ Db/Config.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 //mongoose.connect('mongodb://127.0.0.1:27017/TodoApp');
- mongoose.connect('mongodb+srv://deploydata720:EFV8AxcsXrW3dZM2@cluster0.hi7pxnf.mongodb.net/?retryWrites=true&w=majority');
+ MONGODB_CONNECT_URI='mongodb+srv://deploydata720:EFV8AxcsXrW3dZM2@cluster0.hi7pxnf.mongodb.net/?retryWrites=true&w=majority'
+ mongoose.connect(URL_MONGODB);
  
 
 const db=mongoose.connection;
