3f7295bedcee1ce6b14549c8dd2a8e74f0006fd2
shu2002k <shubhkuttiya@gmail.com>
2023-09-05 19:30:00 -0700

    commite

--- Db/Config.js
+++ Db/Config.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 //mongoose.connect('mongodb://127.0.0.1:27017/TodoApp');
- mongoose.connect('mongodb+srv://shubham:shubham@cluster0.mf5z1mw.mongodb.net/?retryWrites=true&w=majority');
+ mongoose.connect('mongodb+srv://deploydata720:EFV8AxcsXrW3dZM2@cluster0.hi7pxnf.mongodb.net/?retryWrites=true&w=majority');
  
 
 const db=mongoose.connection;
