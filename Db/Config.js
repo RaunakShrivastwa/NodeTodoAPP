c3f3adbabd818a7b3b89f442131d5f0a5efd331a
shu2002k <shubhkuttiya@gmail.com>
2023-09-23 02:52:45 -0700

    commite

--- Db/Config.js
+++ Db/Config.js
@@ -1,20 +1,6 @@
-// const mongoose = require('mongoose');
 const mongoose = require('mongoose');
-//mongoose.connect('mongodb://127.0.0.1:27017/TodoApp');
-//  MONGODB_CONNECT_URI='mongodb+srv://deploydata720:EFV8AxcsXrW3dZM2@cluster0.hi7pxnf.mongodb.net/?retryWrites=true&w=majority'
-//  mongoose.connect(MONGODB_CONNECT_URI);
- 
-
-// const db=mongoose.connection;
-
-// db.on('error',console.error.bind(console,"There is problem with connection"))
-// db.once('open',function(){
-//     console.log("Connection done...")
-// })
-
-
 mongoose.set('strictQuery', true);
-mongoose.connect( 'mongodb+srv://deploydata720:EFV8AxcsXrW3dZM2@cluster0.hi7pxnf.mongodb.net/?retryWrites=true&w=majority', {
+mongoose.connect( '', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
