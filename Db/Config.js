e54b14ad9ffcc372ece45f7bcd83a0da75b5daf0
shu2002k <shubhkuttiya@gmail.com>
2023-09-23 02:55:31 -0700

    commite

--- Db/Config.js
+++ Db/Config.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 mongoose.set('strictQuery', true);
-mongoose.connect( '', {
+mongoose.connect( 'mongodb+srv://deploydata720:EFV8AxcsXrW3dZM2@cluster0.hi7pxnf.mongodb.net/?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
